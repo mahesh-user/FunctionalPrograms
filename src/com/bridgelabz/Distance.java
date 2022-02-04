975b9637183a9df80cc4f027246ae46e0b38f1aa
Mahesh <mahesh51138@gmail.com>
2022-02-05 00:25:49 +0530

    added:CaluculateDistance

--- src/com/bridgelabz/Distance.java
+++ src/com/bridgelabz/Distance.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Distance {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+      Scanner sc = new Scanner(System.in);
         System.out.println(" enter values ");
         int x = sc.nextInt();
 
